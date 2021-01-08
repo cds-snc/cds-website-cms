1cec6da38be75ec2f4434aa5984fcb1de0605ce0
Bethany Dunfield <bethany.dunfield@cds-snc.ca>
2021-01-08 11:28:29 -0700

    change from SQLite to Postgres

--- config/database.js
+++ config/database.js
@@ -4,11 +4,15 @@ module.exports = ({ env }) => ({
     default: {
       connector: 'bookshelf',
       settings: {
-        client: 'sqlite',
-        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+        client: 'postgres',
+        host: env('DATABASE_HOST', '127.0.0.1'),
+        port: env.int('DATABASE_PORT', 5432),
+        database: env('DATABASE_NAME', 'cdswebsitecms'),
+        username: env('DATABASE_USERNAME', 'db_user'),
+        password: env('DATABASE_PASSWORD', 'password'),
       },
       options: {
-        useNullAsDefault: true,
+        ssl: false,
       },
     },
   },
