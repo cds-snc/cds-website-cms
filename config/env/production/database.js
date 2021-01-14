4fef8b48ffbc779e00aa019f00f6d4f351161792
Bethany Dunfield <bethany.dunfield@cds-snc.ca>
2021-01-14 14:02:59 -0700

    change production db file for AWS. This will break Heroku

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,5 +1,4 @@
 const parse = require('pg-connection-string').parse;
-const config = parse(process.env.DATABASE_URL);
 
 module.exports = ({ env }) => ({
   defaultConnection: 'default',
@@ -8,17 +7,17 @@ module.exports = ({ env }) => ({
       connector: 'bookshelf',
       settings: {
         client: 'postgres',
-        host: config.host,
-        port: config.port,
-        database: config.database,
-        username: config.user,
-        password: config.password,
+        host: env('DATABASE_HOST', '127.0.0.1'),
+        port: env.int('DATABASE_PORT', 5432),
+        database: env('DATABASE_NAME', 'strapi'),
+        username: env('DATABASE_USERNAME', ''),
+        password: env('DATABASE_PASSWORD', ''),
         ssl: {
           rejectUnauthorized: false
         }
       },
       options: {
-        ssl: true,
+        ssl: false,
       },
     },
   },
