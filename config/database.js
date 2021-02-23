72a44afcd6c4c1fa13134a1e29ac3ed646d506f3
brdunfield <bethany.dunfield@cds-snc.ca>
2021-02-23 12:35:11 -0700

    add onhomepage boolean to product schema

--- config/database.js
+++ config/database.js
@@ -7,7 +7,7 @@ module.exports = ({ env }) => ({
         client: 'postgres',
         host: env('DATABASE_HOST', '127.0.0.1'),
         port: env.int('DATABASE_PORT', 5432),
-        database: env('DATABASE_NAME', 'cdswebsitecms')
+        database: env('DATABASE_NAME', 'cdswebsitecms'),
         username: env('DATABASE_USERNAME', 'db_user'),
         password: env('DATABASE_PASSWORD', 'password'),
       },
