f6c04a363167a528c1dd28d38fb1685275e86e21
Mohamed Nur <85885638+mohamed-cds@users.noreply.github.com>
2022-06-10 14:10:52 -0400

    feat: retrieve aws keys dynamically using task role (#21)
    * feat: retrieve aws keys dynamically using task role
    
    * feat: use oidc to deploy ecs tasks
    
    * feat: use task role aws creds automatically

--- config/plugins.js
+++ config/plugins.js
@@ -2,8 +2,6 @@ module.exports = ({ env }) => ({
   upload: {
     provider: 'aws-s3',
     providerOptions: {
-      accessKeyId: env('AWS_ACCESS_KEY_ID'),
-      secretAccessKey: env('AWS_ACCESS_SECRET_KEY'),
       region: 'ca-central-1',
       params: {
         Bucket: env('BUCKET_NAME'),
