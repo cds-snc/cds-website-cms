12415ad9d7d9b2a79e6f353905ba6d87e600792a
omartehsin1 <omartehsin@gmail.com>
2024-04-05 14:05:50 -0400

    refactor

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,24 +1,18 @@
 const parse = require('pg-connection-string').parse;
 
 module.exports = ({ env }) => ({
-  defaultConnection: 'default',
-  connections: {
-    default: {
-      connector: 'bookshelf',
-      settings: {
-        client: 'postgres',
-        host: env('DATABASE_HOST', '127.0.0.1'),
-        port: env.int('DATABASE_PORT', 5432),
-        database: env('DATABASE_NAME', 'strapi'),
-        username: env('DATABASE_USERNAME', ''),
-        password: env('DATABASE_PASSWORD', ''),
-        ssl: {
-          rejectUnauthorized: false
-        }
-      },
-      options: {
-        ssl: false,
-      },
+  connection: {
+    client: 'postgres',
+    connection: {
+      host: env('DATABASE_HOST', '127.0.0.1'),
+      port: env.int('DATABASE_PORT', 5432),
+      database: env('DATABASE_NAME', 'strapi'),
+      username: env('DATABASE_USERNAME', ''),
+      password: env('DATABASE_PASSWORD', ''),
+      ssl: {
+        rejectUnauthorized: false
+      }
     },
+    debug: false,
   },
 });
--- config/server.js
+++ config/server.js
@@ -7,3 +7,12 @@ module.exports = ({ env }) => ({
     },
   },
 });
+
+
+// module.exports = ({ env }) => ({
+//   host: env('HOST', '0.0.0.0'),
+//   port: env.int('PORT', 1337),
+//   app: {
+//     keys: env.array('APP_KEYS'),
+//   },
+// });
