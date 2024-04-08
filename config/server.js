47d2d74286511c5abe5655ddab35c5c714e16ed8
omartehsin1 <omartehsin@gmail.com>
2024-04-08 11:15:37 -0400

    WIP

--- config/server.js
+++ config/server.js
@@ -1,11 +1,9 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET', '84d69d082ca568fd62f5887d4b42ab97'),
-    },
-  },
+  app: {
+    keys: env.array('APP_KEYS', ['myKeyA', 'myKeyB'])
+  }
 });
 
 
